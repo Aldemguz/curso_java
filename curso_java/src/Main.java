ae1b26e93ef7d006af1d1773bdeea1a0000419ec
Aldemaro Guzman <aldemaroguzman93@gmail.com>
2018-08-19 20:04:33 -0430

    Creation Employeer Class, Adding 2 Employyers

--- curso_java/src/Main.java
+++ curso_java/src/Main.java
@@ -14,9 +14,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("I am practicing git, fuck shit");
-        System.out.println("Esta es la ultima version, git 2.0 madafaqar");
+        Employeer employeer1 = new Employeer("Aldemaro");
+        Employeer employeer2 = new Employeer("Irina");
+        
+        System.out.println(employeer1.getData());
+        System.out.println(employeer2.getData());
+        
+        System.out.println(Employeer.getNextId());
     }
     
+}
+
+class Employeer{
+    
+    public Employeer(String name){
+        this.name = name;
+        this.section = "Administration";
+        id = nextid;
+        nextid++;
+        
+    }
+    public void setSection(String section){
+        this.section = section;
+    }
+    
+    public String getData(){
+        return "Name: "+name+", section: "+section+", with id: "+id;
+    }
+    
+    public static String getNextId(){
+        return "The next id is: "+nextid;
+    }
+
+private final String name;
+private String section;
+private final int id;
+private static int nextid=1;
 }
